a91d9912801f7bbea7444439e319867f25c211a4
ahamad2 <abdullah.hamad@huskers.unl.edu>
2021-11-28 12:31:16 -0600

    latest changes to display chart.

--- index.js
+++ index.js
@@ -1,50 +1,57 @@
+// const height = 400;
+// const width = 800;
+// const margin = {top:50, bottom:50, left:50, right:20};
+
+const svg = d3.select("svg");
+const margin = 200;
+const width = svg.attr("width") - margin;
+const height = svg.attr("height") - margin;
+
+const x = d3.scaleBand().range([0, width]).padding(0.4);
+const y = d3.scaleLinear().range([height, 0]);
+
+const g = svg.append("g")
+               .attr("transform", "translate(" + 100 + "," + 100 + ")");
+
 d3.tsv('state_population_gdp.tsv', (...data) => {
     // console.log(data);
-    const height = 400;
-    const width = 800;
-    const margin = {top:50, bottom:50, left:50, right:20};
+    data.forEach((d) => {
+        d.population = +d.population;
+        d.gdp = +d.gdp;
+    });
+    // console.log(data);
+    x.domain(data.map(function(d) { return d.name; }));
+    y.domain([0, d3.max(data, function(d) { return d.population; })]);
 
-    const svg = d3.select('#myChart')
-        .append('svg')
-        .attr('width', width - margin.left - margin.right)
-        .attr('height', height - margin.top - margin.bottom)
-        .attr("viewBox", [0, 0, width, height]);
-   
-    const x = d3.scaleBand()
-        .domain(d3.range(data.length))
-        .range([margin.left, width - margin.right])
-        .padding(0.1)
-    
-    const y = d3.scaleLinear()
-        .domain([0, 39000000])
-        .range([height - margin.bottom, margin.top])
-    
-    svg
-        .append("g")
-        .attr("fill", 'royalblue')
-        .selectAll("rect")
-        .data(data.sort((a, b) => d3.descending(a.population, b.population)))
-        .join("rect")
-            .attr("x", (d, i) => x(i))
-            .attr("y", d => y(d.population))
-            .attr('title', (d) => d.population)
-            .attr("class", "rect")
-            .attr("height", d => y(0) - y(d.population))
-            .attr("width", x.bandwidth());
-    
-    function yAxis(g) {
-        g.attr("transform", `translate(${margin.left}, 0)`)
-            .call(d3.axisLeft(y).ticks(null, data.format))
-            .attr("font-size", '20px')
-    }
-    
-    function xAxis(g) {
-        g.attr("transform", `translate(0,${height - margin.bottom})`)
-            .call(d3.axisBottom(x).tickFormat(i => data[i].state))
-            .attr("font-size", '20px')
-    }
-    
-    svg.append("g").call(xAxis);
-    svg.append("g").call(yAxis);
-    svg.node();
+    g.append("g")
+         .attr("transform", "translate(0," + height + ")")
+         .call(d3.axisBottom(x))
+         .append("text")
+         .attr("y", height - 250)
+         .attr("x", width - 100)
+         .attr("text-anchor", "end")
+         .attr("stroke", "black")
+         .text("Name");
+
+    g.append("g")
+    .call(d3.axisLeft(y).tickFormat(function(d){
+        return "$" + d;
+    })
+    .ticks(10))
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "-5.1em")
+    .attr("text-anchor", "end")
+    .attr("stroke", "black")
+    .text("Population");
+
+   g.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.name); })
+    .attr("y", function(d) { return y(d.population); })
+    .attr("width", x.bandwidth())
+    .attr("height", function(d) { return height - y(d.population); });
 });
