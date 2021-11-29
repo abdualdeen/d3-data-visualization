fb7654805f602f27177dcb03091a610ee590ac30
ahamad2 <abdullah.hamad@huskers.unl.edu>
2021-11-28 21:45:57 -0600

    added color change on hover.

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
-// const height = 400;
-// const width = 800;
-// const margin = {top:50, bottom:50, left:50, right:20};
+var svg = d3.select("svg");
+var margin = 200;
+var width = svg.attr("width") - margin;
+var height = svg.attr("height") - margin;
 
-const svg = d3.select("svg");
-const margin = 200;
-const width = svg.attr("width") - margin;
-const height = svg.attr("height") - margin;
+var x = d3.scaleBand().range([0, width]).padding(0.0);
+var y = d3.scaleLinear().range([height, 0]);
 
-const x = d3.scaleBand().range([0, width]).padding(0.4);
-const y = d3.scaleLinear().range([height, 0]);
-
-const g = svg.append("g")
+var g = svg.append("g")
                .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
-d3.tsv('state_population_gdp.tsv', (...data) => {
-    // console.log(data);
+
+function onMouseOver(d, i) {
+    d3.select(this).attr('class', 'highlight');
+        d3.select(this)
+          .transition()
+          .duration(200)
+          .attr('width', x.bandwidth() + 5)
+          .attr("y", function(d) { return y(d.population) - 10; })
+          .attr("height", function(d) { return height - y(d.population) + 10; });
+
+        g.append("text")
+         .attr('class', 'val') 
+         .attr('x', function() {
+             return x(d.state);
+         })
+         .attr('y', function() {
+             return y(d.population) - 15;
+         })
+         .text(function() {
+             return [d.state + ', ' + d.population];
+         });
+}
+
+function onMouseOut(d, i) {
+    // use the text label class to remove label on mouseout
+    d3.select(this).attr('class', 'bar');
+    d3.select(this)
+      .transition()     // adds animation
+      .duration(200)
+      .attr('width', x.bandwidth())
+      .attr("y", function(d) { return y(d.population); })
+      .attr("height", function(d) { return height - y(d.population); });
+
+    d3.selectAll('.val')
+      .remove()
+}
+
+
+
+d3.tsv('state_population_gdp.tsv').then(function(data) {
+    console.log(data);
     data.forEach((d) => {
         d.population = +d.population;
         d.gdp = +d.gdp;
     });
-    // console.log(data);
-    x.domain(data.map(function(d) { return d.name; }));
+    x.domain(data.map(function(d) { return d.state; }));
     y.domain([0, d3.max(data, function(d) { return d.population; })]);
 
     g.append("g")
-         .attr("transform", "translate(0," + height + ")")
-         .call(d3.axisBottom(x))
-         .append("text")
-         .attr("y", height - 250)
-         .attr("x", width - 100)
-         .attr("text-anchor", "end")
-         .attr("stroke", "black")
-         .text("Name");
-
-    g.append("g")
-    .call(d3.axisLeft(y).tickFormat(function(d){
-        return "$" + d;
-    })
-    .ticks(10))
     .append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", 6)
@@ -49,8 +69,10 @@ d3.tsv('state_population_gdp.tsv', (...data) => {
    g.selectAll(".bar")
     .data(data)
     .enter().append("rect")
+    .on("mouseover", onMouseOver) //Add listener for the mouseover event
+    .on("mouseout", onMouseOut)   //Add listener for the mouseout event
     .attr("class", "bar")
-    .attr("x", function(d) { return x(d.name); })
+    .attr("x", function(d) { return x(d.state); })
     .attr("y", function(d) { return y(d.population); })
     .attr("width", x.bandwidth())
     .attr("height", function(d) { return height - y(d.population); });
